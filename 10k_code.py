46ef1a21a7d7f377ff89d194506fa45b98c47b2f
Leon Horne <leonhornejr@gmail.com>
2023-06-21 08:19:28 +0200

    update folder structure layout

---
+++ 10k_code.py
@@ -0,0 +1,39 @@
+# 10K productive and effective lines of code 
+# where every 1000th line of code is better than the last.
+
+def 10kCode_gauge():
+
+# in real time (update every second if possible)
+# scrap files from Leo_Soultions folder
+# get lines of code that user: leo writes
+# count the lines of code
+# return a "total number of lines of code" integer
+# Display the "total number of lines of code" integer on a gauge
+# make a real time desktop widget that displays the gauge
+# make the widget transparent
+
+def 10kCode_db():
+
+# store all lines of code in a database
+# store the date and time of each line of code
+# store the file name of each line of code
+# store the line number of each line of code
+
+def 10kCode_graph():
+
+# graph the 10kCode_db() data
+# x-axis: time
+# y-axis: lines of code
+# 
+
+
+
+
+
+
+
+
+
+
+
+
