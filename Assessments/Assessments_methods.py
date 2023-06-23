703209eac4b7f38a1086d51f30ce65ba8e92ecd0
Leon Horne <leonhornejr@gmail.com>
2023-06-23 08:33:17 +0200

    Create Assessments_methods.py

---
+++ Assessments/Assessments_methods.py
@@ -0,0 +1,6 @@
+1.	Frequent code reviews: This allows for an assessment of the engineer's coding abilities and adherence to best practices1.
+2.	Asking the engineer to review others' code: This can show the engineer's ability to understand and critique code written by others, providing insight into their technical knowledge and collaborative skills1.
+3.	Inspecting their documentation: The quality of an engineer's documentation can be a strong indicator of their attention to detail and ability to communicate complex ideas clearly1.
+4.	Observing how they work in a team: Teamwork is a crucial part of software development, and an engineer's ability to work well in a team can be as important as their individual technical skills1.
+5.	Conducting regular one-on-one meetings: These meetings can provide insight into the engineer's thoughts about the project, its positive and negative aspects, and their overall engagement with their work1.
+6.	Measuring their contributions to improving technology processes, such as CI/CD or training for the team1.
